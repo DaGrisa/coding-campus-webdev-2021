2fd12a2d4fe6ae9d0939a6cdde966d1023ab98bd
daniel <dgr7348@students.fhv.at>
2022-01-21 21:31:37 +0100

    roll the dice

---
+++ roll-the-dice/index.js
@@ -0,0 +1,44 @@
+let history = [];
+
+async function rollTheDice() {
+  const randomNumber = getRandomDiceNumber();
+  await showRollingAnimation();
+  showDiceNumber(randomNumber);
+  refreshAndShowHistory(randomNumber);
+}
+
+/**
+ * @returns random number between 1 and 6
+ */
+function getRandomDiceNumber() {
+  return Math.ceil(Math.random() * 6);
+}
+
+/**
+ * Shows the given number in the dice component.
+ * @param {*} number number to show in dice component
+ */
+function showDiceNumber(number) {
+  document.getElementById("dice").innerHTML = number;
+}
+
+/**
+ * adds the number to the history array and replaces the values in the history component
+ * @param {*} number
+ */
+function refreshAndShowHistory(number) {
+  history = history.slice(0, 10);
+  document.getElementById("history").innerHTML = history;
+  history.unshift(number);
+}
+
+async function showRollingAnimation() {
+  for (let i = 1; i < 9; i++) {
+    showDiceNumber(getRandomDiceNumber());
+    await sleep(i * 50);
+  }
+}
+
+function sleep(milliseconds) {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
