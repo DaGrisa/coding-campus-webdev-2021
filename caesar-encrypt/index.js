32cc2926620e7c85244dd25d05f401f234255976
daniel <dgr7348@students.fhv.at>
2022-02-01 20:28:32 +0100

    caesar encrypt normalize

--- caesar-encrypt/index.js
+++ caesar-encrypt/index.js
@@ -1,5 +1,52 @@
+const supportedCharacters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  " ",
+  ".",
+  "!",
+  "?",
+];
+
 function caesar() {
-  const inputText = document.getElementById("text-input").value;
+  const inputText = normalizeText(document.getElementById("text-input").value);
   const textOutput = inputText;
   document.getElementById("text-output").value = textOutput;
+}
+
+function normalizeText(text) {
+  let normalizedText = "";
+
+  for (let character of text.toLowerCase()) {
+    if (supportedCharacters.includes(character)) {
+      normalizedText += character;
+    } else {
+      normalizedText += "�";
+    }
+  }
+
+  return normalizedText;
 }
