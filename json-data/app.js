4a93e7810b0bed1345878c2250ba70c5006fd7d1
daniel <dgr7348@students.fhv.at>
2022-02-08 20:55:33 +0100

    employee list

---
+++ json-data/app.js
@@ -0,0 +1,32 @@
+async function startApp() {
+  console.log("--- Employee Management ---");
+  const employees = await loadEmployeesAsync();
+  renderEmployeeList(employees);
+}
+
+async function loadEmployeesAsync() {
+  const response = await fetch(
+    "http://127.0.0.1:5500/json-data/employees.json"
+  );
+  const employees = await response.json();
+  return employees;
+}
+
+function renderEmployeeList(employees) {
+  const listElement = document.getElementById("list");
+  for (const employee of employees) {
+    const listItem = document.createElement("div");
+    listItem.className = "list-item";
+
+    const keyItem = document.createElement("div");
+    keyItem.className = "list-item-key";
+    keyItem.innerText = employee.key;
+    listItem.appendChild(keyItem);
+    const nameItem = document.createElement("div");
+    nameItem.className = "list-item-name";
+    nameItem.innerText = `${employee.firstName} ${employee.lastName}`;
+    listItem.appendChild(nameItem);
+
+    listElement.appendChild(listItem);
+  }
+}
--- json-data/list-employees.js
+++ json-data/list-employees.js
@@ -4,4 +4,10 @@ const fileSystem = require("fs");
 
 const rawdata = fileSystem.readFileSync("employees.json");
 const employees = JSON.parse(rawdata);
-console.log(employees);
+
+for (const employee of employees) {
+  const key = employee.key;
+  const firstName = employee.firstName;
+  const lastName = employee.lastName;
+  console.log(`${key} - ${firstName} ${lastName}`);
+}
