f72fc01e57b173af419b994c28813b1e9caaf53c
dag <daniel.griesser@creasoft.li>
2022-04-19 21:48:41 +0200

    react todo hooks

--- todo-react/src/App.tsx
+++ todo-react/src/App.tsx
@@ -1,55 +1,40 @@
 import Todo from './components/Todo';
+import TodoItem from "./models/TodoItem";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import {useState} from "react";
 
-export default function App() {
+export default function App(props: {tasks: TodoItem[]}) {
+    const [tasks, setTasks] = useState<TodoItem[]>(props.tasks);
+
+    function addTask(name: string){
+        const newTask: TodoItem = {id: "task-" + tasks.length, name: name, completed: false};
+        setTasks([...tasks, newTask]);
+    }
+
+    const taskList = tasks.map(task => (
+        <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />
+    ));
+
   return (
     <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
+        <h1>TodoMatic</h1>
+        <Form addTask={addTask}/>
+        <div className="filters btn-group stack-exception">
+            <FilterButton name="all" active={true}/>
+            <FilterButton name="Active" active={false}/>
+            <FilterButton name="Completed" active={false}/>
+        </div>
+        <h2 id="list-heading">
+            3 tasks remaining
         </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
-      <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-      </div>
-      <h2 id="list-heading">
-        3 tasks remaining
-      </h2>
-      <ul
-        role="list"
-        className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading"
-      >
-        <Todo />
-        <Todo />
-        <Todo />
-      </ul>
+        <ul
+            role="list"
+            className="todo-list stack-large stack-exception"
+            aria-labelledby="list-heading"
+        >
+            {taskList}
+        </ul>
     </div>
   );
 }
--- todo-react/src/index.tsx
+++ todo-react/src/index.tsx
@@ -4,9 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const DATA = [
+    { id: "todo-0", name: "Eat", completed: true },
+    { id: "todo-1", name: "Sleep", completed: false },
+    { id: "todo-2", name: "Repeat", completed: false }
+];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App tasks={DATA} />
   </React.StrictMode>,
   document.getElementById('root')
 );
